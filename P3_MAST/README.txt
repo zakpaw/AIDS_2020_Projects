cb956d24c31ba22c48d42cf074ce4ae0f2c52d0d
zakpaw <zakpaw36@gmail.com>
2020-10-04 14:52:37 +0200

    added P4

--- P3_MAST/README.txt
+++
@@ -1,1 +0,0 @@
-The exercise is to implement an algorithm that reads a set of n rooted trees in the NEWICK format, and then for each of n(n-1)/2 pairs of trees will calculate the minimum number of leaves whose removal from both trees will make them isomorphic. The problem is known in IT as Maximum agreement subtree problem .
--- P4_The_Plague/main.cpp
+++ P4_The_Plague/main.cpp
@@ -4,12 +4,8 @@
 
 int main()
 {
-    //clock_t time = clock();
-    freopen("t2.txt", "r", stdin);
+    freopen("sample_input.txt", "r", stdin);
     Plague plague = input_plague();
-    //time = clock() - time;
     plague.run();
-    //cout << "run: " << double(clock() - time) / CLOCKS_PER_SEC << endl;
-
     return 0;
 }
